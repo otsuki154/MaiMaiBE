3aee4a19cfddfc3c630886ba8b3adfec0573577e
VAN THANH <VAN THANH@10.251.11.155>
2021-05-18 21:34:48 +0900

    xoa file thua

--- src/main/java/com/maimai/Master/Grammar/GrammarController.java
+++ src/main/java/com/maimai/Master/Grammar/GrammarController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.maimai.Utils.CsvDownload;
 import com.maimai.Utils.CsvUpload;
 
 @RestController
